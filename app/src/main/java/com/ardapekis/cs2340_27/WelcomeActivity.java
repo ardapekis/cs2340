b471d184135429b1b861e0d4c8f29a64f7f6d942
Jason Lee <jason.khn.lee@gmail.com>
2017-09-17 23:43:12 -0400

    Added a modern welcome screen

---
+++ app/src/main/java/com/ardapekis/cs2340_27/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.ardapekis.cs2340_27;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
