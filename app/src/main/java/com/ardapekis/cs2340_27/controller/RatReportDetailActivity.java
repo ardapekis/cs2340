094e0c7229e982081f6f89756aa46d2985d15b7e
Jason Lee <jason.khn.lee@gmail.com>
2017-10-05 10:56:02 -0400

    Added new classes, empty btw

---
+++ app/src/main/java/com/ardapekis/cs2340_27/controller/RatReportDetailActivity.java
@@ -0,0 +1,8 @@
+package com.ardapekis.cs2340_27.controller;
+
+/**
+ * Created by jason on 10/5/2017.
+ */
+
+public class RatReportDetailActivity {
+}
---
+++ app/src/main/java/com/ardapekis/cs2340_27/model/RatReportManager.java
@@ -0,0 +1,8 @@
+package com.ardapekis.cs2340_27.model;
+
+/**
+ * Created by jason on 10/5/2017.
+ */
+
+public class RatReportManager {
+}
