97f1581b412419076fe9e6f695100921cce3e024
David Herszenhaut <david.herszenhaut@gmail.com>
2017-11-12 03:56:47 -0500

    Began working on JUnit for findItemByKey()

---
+++ app/src/test/java/com/ardapekis/cs2340_27/DavidHerszenhautTest.java
@@ -0,0 +1,25 @@
+package com.ardapekis.cs2340_27;
+
+import com.ardapekis.cs2340_27.model.RatReportItem;
+import com.ardapekis.cs2340_27.model.RatReportManager;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by David on 11/12/2017.
+ */
+
+public class DavidHerszenhautTest {
+
+    @Test
+    public void testFindItemByKey() {
+
+        RatReportManager ratReportManager = new RatReportManager();
+        // make key, createdDate, location, and author to initialize RatReportItem
+        RatReportItem ratReportItem = new RatReportItem();
+        ratReportManager.addItem();
+
+    }
+
+}
