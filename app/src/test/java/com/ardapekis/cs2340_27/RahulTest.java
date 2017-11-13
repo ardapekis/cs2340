f3cfd07293d19ed79309f3cfbd87481dabd53e37
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2017-11-13 17:10:10 -0500

    Added Rahul's junit test

---
+++ app/src/test/java/com/ardapekis/cs2340_27/RahulTest.java
@@ -0,0 +1,24 @@
+package com.ardapekis.cs2340_27;
+
+import com.ardapekis.cs2340_27.model.Facade;
+import com.ardapekis.cs2340_27.model.User;
+import com.ardapekis.cs2340_27.model.UserManager;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RahulTest {
+    @Test
+    public void addUser_isCorrect() throws Exception {
+        Assert.assertTrue(Facade.getInstance().getUserManager().addUser(new User()));
+        Assert.assertFalse(Facade.getInstance().getUserManager().addUser(new User()));
+    }
+}
