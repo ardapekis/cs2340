c7a73296667ced4d15eba98a95ffd42a5bc4d69e
Austin Jang <jjang77@gatech.edu>
2017-11-20 22:46:09 -0500

    fixed branch coverage on austintest

--- app/src/test/java/com/ardapekis/cs2340_27/AustinTest.java
+++ app/src/test/java/com/ardapekis/cs2340_27/AustinTest.java
@@ -1,6 +1,8 @@
 package com.ardapekis.cs2340_27;
 
 import org.junit.Test;
+
+import com.ardapekis.cs2340_27.model.RatReportItem;
 import com.ardapekis.cs2340_27.model.User;
 
 import static org.junit.Assert.*;
@@ -15,9 +17,10 @@ public class AustinTest {
         User user1 = new User("username", "password");
         User user2 = new User("username", "password1");
         User user3 = new User("username1", "password");
-        String fakeUser = "username";
+        String fakeuser = "username";
+        assertEquals(false, user1.equals(fakeuser));
+        assertEquals(true, user1.equals(user1));
         assertEquals(true, user1.equals(user2));
-        assertEquals(false, user1.equals(fakeUser));
         assertEquals(false, user1.equals(user3));
     }
 }
